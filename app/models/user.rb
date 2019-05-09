feb1e998f9cf3273f73a000059d0fef07d564eb4
Aaron Collins <aa.collins.95@gmail.com>
2019-05-09 16:20:20 -0400

    remembers user before it creates them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  before_create :remember
   has_secure_password
+
+  def remember
+    remember_token = SecureRandom.urlsafe_base64
+    self.remember_digest = Digest::SHA1.hexdigest(remember_token)
+  end
+
+
 end
